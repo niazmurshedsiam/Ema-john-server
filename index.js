d1a8894871a778cc8de1a75271c91e59f7a310f1
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-31 13:31:24 +0600

    insert bulk data many data to the database

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const express = require('express')
+require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm8mo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-require('dotenv').config()
 const app = express()
 const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors());
 const port = 5000
-console.log(process.env.DB_USER);
+ console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const products = client.db("emajohnStore").collection("products");
-  // perform actions on the collection object
+    console.log(err);
+  const productsCollection = client.db("emajohnStore").collection("products");
   app.post('/addProduct',(req,res)=>{
-    const product = req.body;  
-    products.insertOne(product)
+    const products = req.body;
+     console.log(products); 
+    productsCollection.insertMany(products)
     .then(result =>{
         console.log(result);
+        console.log(result.insertedCount);
+        res.send(result.insertedCount);
     })
   })
-  console.log(process.env.DB_NAME,process.env.DB_PASS,process.env.DB_USER);
+//   console.log(process.env.DB_NAME,process.env.DB_PASS,process.env.DB_USER);
 });
 
 
