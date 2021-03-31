5efb114125223361c771113e50098de7d419b750
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-31 22:50:27 +0600

    load data from the server

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ client.connect(err => {
         res.send(result.insertedCount);
     })
   })
+  app.get('/products',(req,res)=>{
+      productsCollection.find({})
+      .toArray((err,documents)=>{
+          res.send(documents);
+      })
+  })
+  app.get('/product/:key',(req,res)=>{
+    productsCollection.find({key: req.params.key})
+    .toArray((err,documents)=>{
+        res.send(documents[0]);
+    })
+})
 //   console.log(process.env.DB_NAME,process.env.DB_PASS,process.env.DB_USER);
 });
 
